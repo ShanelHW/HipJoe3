555a1c6e3a6a3add21fd550f2acc166197bf3f69
Shanel Davis <nshanelh@gmail.com>
2020-08-18 16:46:18 -0400

    menu items update

---
+++ hipjoe2/src/MenuItems.js
@@ -0,0 +1,49 @@
+const drinkItems = [...coffee, ...tea];
+
+const eatItems = [...soup, ...sandwich, ...bakery];
+
+const coffee = [
+    { type: "kona cold brew", temperature: "iced", strength: "strong", size: "" },
+
+    { type: "french press", temperature: "hot", strength: "medium", size: "" },
+
+    {
+        type: "nitro brew",
+        temperature: "cold",
+        strength: "strong",
+        size: "",
+    },
+
+    { type: "espresso", temperature: "hot", strength: "strong", size: "shot" },
+
+    {
+        type: "golden brew",
+        temperature: "hot",
+        strength: "decaf",
+        size: "",
+    },
+];
+
+const tea = [
+    { type: "gunpowder green", temperature: "", strength: "", size: "" },
+
+    { type: "matcha green", temperature: "", strength: "", size: "" },
+
+    { type: "earl grey", temperature: "", strength: "hot", size: "" },
+
+    {
+        type: "english breakfast",
+        temperature: "hot",
+        strength: "strong",
+        size: "",
+    },
+
+    { type: "mint", temperature: "hot", strength: "decaf", size: "" },
+
+    {
+        type: "spiced hibiscus",
+        temperature: "",
+        strength: "decaf",
+        size: "",
+    },
+];
